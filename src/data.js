bf612ba1a50227f81d00a8f9538e934923ffa004
Justin Livi <justin@brinkbit.com>
2016-12-01 10:10:22 -0500

    Inital commit

---
+++ src/data.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    title: 'Community',
+    year: '2011',
+    uri: 'community',
+    source: {
+        owner: 'justinlivi',
+        repo: 'community',
+        ref: 'master',
+        embed: [
+            {
+                type: 'js',
+                label: 'render.js',
+                path: 'src/render.js',
+            },
+        ],
+    },
+};
